10396694ac63c76b2b21770b459e14ada255c274
GernotMaier <gernot.maier@desy.de>
2019-10-23 11:13:02 +0200

    astyle changes

--- inc/VAtmosAbsorption.h
+++ inc/VAtmosAbsorption.h
@@ -26,35 +26,35 @@ using namespace std;
 
 class VAtmosAbsorption
 {
-   private:
-     string fModel;                   //!< used model (CORSIKA,kascade)
-     string fSourceFile;
-
-     double fObservationLevel;        //!< observation height in [m]
-
-     double fminWave;                 //!< minimum wavelength in [nm]
-     double fmaxWave;                 //!< maximum wavelength in [nm]
-     TRandom3 *fRandom;                //!< random generator
-
-     map< int, vector<double> > fCoeff;   //!<   vector with atmospheric data (CORSIKA)
-     map< int, double > fCoeffObs;        //!<   vector with atmospheric data at observation level (CORSIKA)
-
-     vector< vector<double> > extint;     //!<   vector with atmospheric data (kascade)
-
-     double getLinearInterpolate( double x, double x0, double x1, double y0, double y1 );  //!< linear interpolation
-     void   readCorsikaAtmabs();                  //!< read CORSIKA atmospheric extinction file (atmabs.dat)
-     void   read_extint( int );                        //!< read kascade atmospheric extinction file (kextint.dat)
-     void   read_extint_F2( int );                        //!< read kascade atmospheric extinction file (kextint.dat)
-     void   read_extint_M5(  );                        //!< read henrikes atmospheric extinction file (modtran 5)
-     
-   public:
-      VAtmosAbsorption( string, int, string iSourceFile = "" );
-     ~VAtmosAbsorption() {}        
-      void setWavelengthintervall( double iminwavelength, double imaxwavelength );   //!< in [nm]
-      void setObservationlevel( double iobslevel );  //!< in [m]
-      double probAtmAbsorbed( double wavelength, double emissionheigth, double emissionangle );   //!< calculates survival probability for photon
-      double probAtmAbsorbed( double wavelength, double emissionheigth, double emissionangle, double &obsdepth );   //!< calculates survival probability for photon
-      double getWavelength( double emissionheigth, double emissionangle );  //!< get random wavelength 
+    private:
+        string fModel;                   //!< used model (CORSIKA,kascade)
+        string fSourceFile;
+        
+        double fObservationLevel;        //!< observation height in [m]
+        
+        double fminWave;                 //!< minimum wavelength in [nm]
+        double fmaxWave;                 //!< maximum wavelength in [nm]
+        TRandom3* fRandom;                //!< random generator
+        
+        map< int, vector<double> > fCoeff;   //!<   vector with atmospheric data (CORSIKA)
+        map< int, double > fCoeffObs;        //!<   vector with atmospheric data at observation level (CORSIKA)
+        
+        vector< vector<double> > extint;     //!<   vector with atmospheric data (kascade)
+        
+        double getLinearInterpolate( double x, double x0, double x1, double y0, double y1 );  //!< linear interpolation
+        void   readCorsikaAtmabs();                  //!< read CORSIKA atmospheric extinction file (atmabs.dat)
+        void   read_extint( int );                        //!< read kascade atmospheric extinction file (kextint.dat)
+        void   read_extint_F2( int );                        //!< read kascade atmospheric extinction file (kextint.dat)
+        void   read_extint_M5( );                         //!< read henrikes atmospheric extinction file (modtran 5)
+        
+    public:
+        VAtmosAbsorption( string, int, string iSourceFile = "" );
+        ~VAtmosAbsorption() {}
+        void setWavelengthintervall( double iminwavelength, double imaxwavelength );   //!< in [nm]
+        void setObservationlevel( double iobslevel );  //!< in [m]
+        double probAtmAbsorbed( double wavelength, double emissionheigth, double emissionangle );   //!< calculates survival probability for photon
+        double probAtmAbsorbed( double wavelength, double emissionheigth, double emissionangle, double& obsdepth );   //!< calculates survival probability for photon
+        double getWavelength( double emissionheigth, double emissionangle );  //!< get random wavelength
 };
 
 #endif
--- inc/VGrisu.h
+++ inc/VGrisu.h
@@ -21,36 +21,45 @@ using namespace std;
 
 class VGrisu
 {
-   private:
-     bool bSTDOUT;                        //!< write output to stdout
-     ofstream of_file;                    //!< output file
-     map<int,int> particles;              //!< particle ID transformation matrix: first: CORSIKA ID, second: kascade ID
-     float degrad;                        //!< convertion deg->rad
-     int primID;                          //!< primary particle ID
-     double xoff;                         //!< offset in x-coordinate for scattered core
-     double yoff;                         //!< offset in y-coordinate for scattered core
-     double qeff;                         //!< global quantum efficiency
-     double observation_height;           //!< observation height [m]
-
-     int atm_id;			  //!< corsika atmprof number (for depth calculation)
-     
-
-     string fVersion;
-
-     void transformCoord( float&, float&, float& );     //!< transform from CORSIKA to GrIsu coordinates
-     void makeParticleMap();              //!< make map with  particle ID transformation matrix
-     float redang( float );             //! reduce large angle to intervall 0, 2*pi
-
-   public:
-      VGrisu( string fVersion = "", int id = -1 );
-     ~VGrisu() {}
-      void setOutputfile( string );       //!< create grisu readable output file
-      void setObservationHeight( double ih ) { observation_height = ih; }  //!< set observation height
-      void setQeff( double iq ) { qeff = iq; } //!< set global quantum efficiency
-      void setQueff( double iq ) { qeff = iq; } //!< set global quantum efficiency
-      void writeRunHeader( float*, VCORSIKARunheader* );      //!<  write some information about CORSIKA run intot the runheader
-      void writeEvent( telescope_array, bool );  //!< write MC information
-      void writePhotons( bunch, int );         //!< write next photon to grisu file ("P" line)
+    private:
+        bool bSTDOUT;                        //!< write output to stdout
+        ofstream of_file;                    //!< output file
+        map<int, int> particles;             //!< particle ID transformation matrix: first: CORSIKA ID, second: kascade ID
+        float degrad;                        //!< convertion deg->rad
+        int primID;                          //!< primary particle ID
+        double xoff;                         //!< offset in x-coordinate for scattered core
+        double yoff;                         //!< offset in y-coordinate for scattered core
+        double qeff;                         //!< global quantum efficiency
+        double observation_height;           //!< observation height [m]
+        
+        int atm_id;			  //!< corsika atmprof number (for depth calculation)
+        
+        
+        string fVersion;
+        
+        void transformCoord( float&, float&, float& );     //!< transform from CORSIKA to GrIsu coordinates
+        void makeParticleMap();              //!< make map with  particle ID transformation matrix
+        float redang( float );             //! reduce large angle to intervall 0, 2*pi
+        
+    public:
+        VGrisu( string fVersion = "", int id = -1 );
+        ~VGrisu() {}
+        void setOutputfile( string );       //!< create grisu readable output file
+        void setObservationHeight( double ih )
+        {
+            observation_height = ih;    //!< set observation height
+        }
+        void setQeff( double iq )
+        {
+            qeff = iq;    //!< set global quantum efficiency
+        }
+        void setQueff( double iq )
+        {
+            qeff = iq;    //!< set global quantum efficiency
+        }
+        void writeRunHeader( float*, VCORSIKARunheader* );      //!<  write some information about CORSIKA run intot the runheader
+        void writeEvent( telescope_array, bool );  //!< write MC information
+        void writePhotons( bunch, int );         //!< write next photon to grisu file ("P" line)
 };
 
 #endif
